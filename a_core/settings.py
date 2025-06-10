67f0c7a4f9a20c6b0c21b108bdc0b9e3864a9b42
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-10 12:27:27 +0100

    Setup user profile and edit pages

--- a_core/settings.py
+++ a_core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
     'a_home',
     'a_users',
 ]
@@ -119,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- a_core/urls.py
+++ a_core/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from a_home.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
+    path('profile/', include('a_users.urls')),
 ]
+
+# only used in development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
